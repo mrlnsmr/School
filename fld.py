0b950b7af5922a91087757735595e8543a0881e5
mrlnsmr <marlen.schumer@web.de>
2023-12-02 23:55:19 +0100

    made it better

--- fld.py
+++ fld.py
@@ -6,7 +6,6 @@ print("")
 a = int(input("Wie lang ist die Seite a? "))
 b = int(input("Wie lang ist die Seite b? "))
 c = int(input("Wie lang ist die Seite c? "))
-print(type(a))
 
 s = (a+b+c)/2
 A = (sqrt(s*(s-a)*(s-b)*(s-c)))
